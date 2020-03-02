c59fb62b94eeab1f93bb45153e42250af62fcd25
Rubel2475 <37175213+Rubel2475@users.noreply.github.com>
2020-03-02 20:52:01 +0600

    Another approach added

--- Final labwork/CaseConversion_String__1(a).asm
+++ Final labwork/CaseConversion_String__1(a).asm
@@ -22,14 +22,12 @@ org 100h
             MOV AH,9       
             LEA DX,prompt
             INT 21H 
-            
-             
-             
+           
             MOV SI,0
             MOV AH,1            
             LOOP1:
                 INT 21H
-                CMP AL,0DH
+                CMP AL,0DH  ;0DH is ascii for enter key
                 JZ NEXT
                 MOV str[SI],AL
                 INC SI  
@@ -49,15 +47,13 @@ org 100h
                 LOOP2:
                     INT 21H                    
                     MOV CL,str[SI] 
-                    
-                    
+                                  
                     CMP CL,65
                     JL ignore                    
                     CMP CL,122
                     JG ignore 
                     
-                    
-                    XOR CL,32
+                    XOR CL,32       ;32=00100000 , 1 XOR 1 = 0, 0 XOR 0 = 0, 1 XOR 0 = 1 
                     MOV DL,CL
                     
                     CMP SI,BX
@@ -84,6 +80,120 @@ org 100h
                 
 
 ret
+
+
+
+
+
+
+
+;An alternative solution
+
+
+
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+
+.MODEL SMALL
+.STACK 100H
+
+    .DATA
+        str DB 50
+        Iprompt DW 'Input : $'
+        Oprompt DW 'Output: $' 
+    
+    .CODE
+        
+        MAIN PROC
+            MOV AX,@DATA
+            MOV DS,AX
+            
+            
+            LEA DX,Iprompt
+            MOV AH,9
+            INT 21H 
+            
+            MOV SI,0
+            
+            MOV AH,1           
+            loop1:
+                INT 21H
+                
+                CMP AL,0DH
+                JZ next 
+                
+                MOV str[SI],AL   
+                
+                INC SI
+                JMP loop1          
+            
+            next: 
+                MOV BX,SI
+                
+                MOV AH,2
+                MOV DL,0AH
+                INT 21H
+                MOV DL,0DH
+                INT 21H            
+              
+                LEA DX,Oprompt
+                MOV AH,9
+                INT 21H
+                      
+                MOV SI,0
+                      
+                MOV AH,2
+                loop2:
+                    
+                    CMP BX,SI
+                    JZ exit
+                    
+                    MOV CL,str[SI]
+                    
+                    CMP CL,65
+                    JL ignore
+                    CMP CL,122
+                    JG ignore 
+                    
+                    CMP CL,96
+                    JG XR 
+                    
+                    CMP CL,90
+                    JG ignore
+                    
+                    XR:
+                       XOR CL,32
+                       
+                       MOV DL,CL
+                       INT 21H                      
+                         
+                    INC SI
+                    JMP loop2
+                    
+                ignore:      
+                    MOV DL,CL
+                    INT 21H
+                    
+                    INC SI
+                    JMP loop2
+                           
+             exit:
+                MOV AH,4CH
+                INT 21H
+                       
+     
+        MAIN ENDP
+    
+    
+    END MAIN
+
+ret
+
+
 
 
 
